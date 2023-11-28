2960db29c605495ed448b66a523bd9f5ca986967
juan guzman <jmguzman1@uc.cl>
2023-11-28 16:07:41 -0300

    interaction correction

--- src/components/Asistencia.tsx
+++ src/components/Asistencia.tsx
@@ -6,14 +6,15 @@ function Asistencia() {
 				Esperamos compartir este gran día junto a ti.{" "}
 				<strong>Por favor responde antes del 15 de Febrero de 2023.</strong>
 			</p>
-			{/* Embed Google Form using an iframe */}
-			<iframe
-				title="Google Form"
-				src="https://docs.google.com/forms/d/e/1FAIpQLSch_OLzAoTTNRDcxf_YLGjJ_4-2Wdvh5pkF3w_82RxRYmD_2A/viewform"
-				width="100%"
-				height="1500">
-				Cargando…
-			</iframe>
+			<div className="block">
+				<iframe
+					title="Google Form"
+					src="https://docs.google.com/forms/d/e/1FAIpQLSch_OLzAoTTNRDcxf_YLGjJ_4-2Wdvh5pkF3w_82RxRYmD_2A/viewform"
+					width="100%"
+					height="1500">
+					Cargando…
+				</iframe>
+			</div>
 		</>
 	);
 }
